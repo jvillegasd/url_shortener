6a6a93a8d976976c3cb9618037ca81a3d36b63b1
C9-LinkRs <linkrsfan@gmail.com>
2020-01-12 07:16:43 +0100

    Redirect feature when hash exists added

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import redis
 import json
 import string
 from random import choice
-from flask import request
+from flask import request, redirect
 from flask_restplus import Namespace, Resource, abort, fields
 from app.db.db_connection import redis_params
 
@@ -11,21 +11,31 @@ add_url_model = main_namespace.model('Add url', {'url': fields.String})
 redis_db = redis.StrictRedis(**redis_params)
 
 @main_namespace.route('/')
+@main_namespace.route('/<hash>')
 class Home(Resource):
-  def get(self):
-    return redis_db.hgetall('links')
+  def get(self, hash=None):
+    if hash == 'db':
+      return redis_db.hgetall('links')
+    elif not hash:
+      return {'message': 'hello to url shortener'}
+    else:
+      url = hashToUrl(hash)
+      if url:
+        return redirect(url, code=302)
+      else:
+        return {'message': 'url does not exists'}
 
 @main_namespace.route('/short')
-class short(Resource):
+class Short(Resource):
   @main_namespace.expect(add_url_model)
   def post(self):
     params = request.get_json()
     url = params['url']
-    new_url = hashUrl()
+    new_url = getHash()
     redis_db.hset('links', new_url, url)
     return {'new_url': new_url}
 
-def hashUrl():
+def getHash():
   letters = string.ascii_lowercase + string.ascii_uppercase
   new_url = ''
   while True:
@@ -34,3 +44,7 @@ def hashUrl():
     if not result:
       break
   return new_url
+
+def hashToUrl(hash):
+  result = redis_db.hget('links', hash)
+  return result
