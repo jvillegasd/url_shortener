265c42dfbffaee5d180453316d10e95006aea172
C9-LinkRs <linkrsfan@gmail.com>
2020-01-12 18:18:52 +0100

    creating job feature

--- app/jobs/delete_keys.py
+++ app/jobs/delete_keys.py
@@ -9,4 +9,8 @@ redis_db = redis.StrictRedis(**redis_params)
 @job_namespace.route('/')
 class Job(Resource):
   def get(self):
+    current_time = datetime.today()
+    for hash, date in redis_db.hgetall('time'):
+      print(hash)
+      print(date)
     return {'message': 'job done'}
